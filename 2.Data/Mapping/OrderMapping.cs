4410fb441b9ecbe3ff2f1441f7147af05617c2f0
Duy Huynh <huynhtruongdyu@gmail.com>
2020-10-10 03:06:22 +0700

    Optimize Code

--- 2.Data/Mapping/OrderMapping.cs
+++ 2.Data/Mapping/OrderMapping.cs
@@ -1,14 +1,9 @@
 ﻿using _1.Core.Domain;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _2.Data.Mapping
 {
-    public class OrderMapping:EntityTypeConfiguration<Order>
+    public class OrderMapping : EntityTypeConfiguration<Order>
     {
         public OrderMapping()
         {
